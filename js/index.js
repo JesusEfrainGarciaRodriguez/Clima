f7b412cf965f6af4b9ca1786695a84a633dcaa14
JesusEfrainGarciaRodriguez <66291025+JesusEfrainGarciaRodriguez@users.noreply.github.com>
2021-03-29 18:38:24 -0700

    Validaciones js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 function consultar() {
-    document.getElementById('form').classList.add('d-none');
-    document.getElementById('card').classList.remove('d-none');
     var ciudad = document.getElementById('ciudad');
+    if (ciudad.value.length){
+        console.log(ciudad.value);
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+ciudad.value+'&appid=12c49ef835196bec77edd6ef3fa8830c')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            document.getElementById('form').classList.add('d-none');
+            document.getElementById('card').classList.remove('d-none');
+        })
+        .catch(error => {
+            
+        });
+    }
+    
+}
 
-    console.log(ciudad.value);
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Mazatlan&appid=12c49ef835196bec77edd6ef3fa8830c')
-    .then(response => response.json())
-    .then(json => console.log(json));
+function peticion(ciudad) {
+    
 }
 
 function regresar() {
